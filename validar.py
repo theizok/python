51affd2845963eaa629db0b2d48cbe58de6d0827
theizok <ramirezizok@gmail.com>
2023-03-18 12:07:02 -0500

    Actualizacion de nuevos programas

--- validar.py
+++ validar.py
@@ -3,32 +3,38 @@
 #FECHA: Marzo 18/2023
 ##DESCRIPCION: Programa que valida nombe, edad y correo
 #======================================================
-menu = """"
-Bienvenidos al registro de usuarios, llene los campos que le soliciten y seleccione un numero del 1 al 3:
-[1]Digitar su Nombre
-[2]Digitar su edad
-[3]Digitar su correo 
-"""
-print(menu)
-opcion = input('Ingrese la opcion 1,2 o 3  ')
+while True:
+    menu = """"
+    Bienvenidos al registro de usuarios, llene los campos que le soliciten y seleccione un numero del 1 al 3:
+    [1]Digitar su Nombre
+    [2]Digitar su edad
+    [3]Digitar su correo 
+    [4]Cerrar Programa
+    """
+
+    print(menu)
+    opcion = input('Ingrese la opcion 1, 2, 3, 4  ')
 #=================Opcion 1 Nombre ==================
-if opcion == "1":
-    nombre = input('Ingrese su nombre   ')
-    if nombre.isalpha():
-        print("Su nombre es:  ", nombre)
-    else:
-        print("Su nombre esta mal digitado  ")
+    if opcion == "1":
+        nombre = input('Ingrese su nombre   ')
+        if nombre.isalpha():
+            print("Su nombre es:  ", nombre)
+        else:
+            print("Su nombre esta mal digitado  ")
 #===================Opcion 2 Edad==================        
-elif opcion == "2":
-    edad = input("Ingrese su edad  ")
-    if edad.isnumeric():
-        print("Su edad es de ", edad , "años " )
-    else:
-        print("Su edad esta digitada de una manera incorrecta")
+    elif opcion == "2":
+        edad = input("Ingrese su edad  ")
+        if edad.isnumeric():
+            print("Su edad es de ", edad , "años " )
+        else:
+            print("Su edad esta digitada de una manera incorrecta")
 #=================Opcion 3 Correo =================
-elif opcion == "3":
-    correo = input('Ingrese su correo  ')
-    if correo.find('@') == 1 and correo.find('.') == 1:
-        print("Tu correo es ", correo ) 
-    else:
-        print('Digitaste erroneamente tu correo')
+    elif opcion == "3":
+        correo = input('Ingrese su correo  ')
+        if correo.find('@') >=0 and correo.find('.') >=1:
+            print("Tu correo es ", correo ) 
+        else:
+            print('Digitaste erroneamente tu correo')
+#===============Opcion 4 Salir =====================
+    elif opcion =="4":
+        exit()
